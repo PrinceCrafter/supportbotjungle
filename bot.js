802266e47b44e760894c216d2ef537b3cc1268b4
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-08-07 12:38:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,4 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
