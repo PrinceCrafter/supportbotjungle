5aa09ae56d65b531f9af162c62815f339a05d52f
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-08-07 12:37:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on('ready', () => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Codes] ${client.users.size}`)
-    client.user.setStatus("idle")
-client.user.setStatus("Online")
-client.user.setGame(`${prefix}help | ${client.users.size} Users | Jungle Network`, "https://twitch.tv/Testing")
-});
+
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
